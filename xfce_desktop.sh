7a19e15be415d0bffb9cf0b9ada38b79faeb3dbc
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2022-10-06 20:24:08 +0200

    Update xfce_desktop.sh
    Install sctd as blue light suppressor

--- xfce_desktop.sh
+++ xfce_desktop.sh
@@ -33,7 +33,7 @@ echo "Installing XFCE and creative suite..."
 
 echo ""
 
-pkg install -y xorg slim xfce xfce4-pulseaudio-plugin thunar-archive-plugin xarchiver unzip sudo bash wget htop gnome-keyring xfce4-screenshooter-plugin ristretto gnome-font-viewer mixer mixertui nano baobab networkmgr v4l-utils v4l_compat webcamd pwcview
+pkg install -y xorg slim xfce xfce4-pulseaudio-plugin thunar-archive-plugin xarchiver unzip sudo bash wget htop gnome-keyring xfce4-screenshooter-plugin ristretto gnome-font-viewer mixer mixertui nano baobab networkmgr v4l-utils v4l_compat webcamd pwcview sctd
 
 pkg install -y vlc audacious audacious-plugins audacity ardour6 guitarix-lv2 lmms hydrogen yoshimi qjackctl firefox krita shotcut filezilla vscode
 
