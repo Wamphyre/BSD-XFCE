46eaa754cfd06a1e3c823338fb7a21b37c940f9e
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2022-10-10 09:58:29 +0200

    Update xfce_desktop.sh

--- xfce_desktop.sh
+++ xfce_desktop.sh
@@ -10,69 +10,64 @@ echo "This will install a complete, secure and optimized XFCE desktop in your Fr
 echo "WARNING!! - Execute only in a fresh vanilla installation"
 
 sleep 5
-
+## CHANGE FreeBSD REPOS TO LATEST
 sed -i '' 's/quarterly/latest/g' /etc/pkg/FreeBSD.conf
 
+## REBUILD AND UPDATE PKG DATABASE 
 echo "Upgrading packages..."
-
 echo ""
-
 pkg update && pkg upgrade -y
-
 echo ""
 
+## FETCH FreeBSD PORTS
 echo "Downloading Ports tree...";
-
 echo ""
-
 portsnap fetch auto
-
 echo ""
 
+## INSTALLS BASE DESKTOP AND CORE UTILS
 echo "Installing XFCE and creative suite..."
-
 echo ""
+pkg install -y nano sudo bash wget htop xorg slim xfce xfce4-pulseaudio-plugin thunar-archive-plugin xarchiver unzip 
+pkg install -y gnome-keyring xfce4-screenshooter-plugin ristretto atril-lite gnome-font-viewer mixer mixertui  
+pkg install -y baobab networkmgr v4l-utils v4l_compat webcamd pwcview sctd brut clamtk filezilla vscode firefox
 
-pkg install -y xorg slim xfce xfce4-pulseaudio-plugin thunar-archive-plugin xarchiver unzip sudo bash wget htop gnome-keyring xfce4-screenshooter-plugin ristretto atril-lite gnome-font-viewer mixer mixertui nano baobab networkmgr v4l-utils v4l_compat webcamd pwcview sctd brut clamtk
-
-pkg install -y vlc deadbeef audacity ardour6 guitarix-lv2 hydrogen yoshimi qjackctl firefox krita shotcut filezilla vscode
-
+## COMPILE AND INSTALLS CREATIVE SUITE APPLICATIONS
+pkg install -y vlc krita shotcut 
 cd /usr/ports/audio/lsp-plugins-lv2 && make install clean BATCH=YES
 cd /usr/ports/audio/lmms && make install clean OPTIONS="WITH_JACK WITH_PULSEAUDIO" BATCH=YES
+cd /usr/ports/audio/ardour6 && make install clean BATCH=YES
+cd /usr/ports/audio/qjackctl && make install clean BATCH=YES
+cd /usr/ports/audio/audacity && make install clean BATCH=YES
+cd /usr/ports/audio/guitarix-lv2 && make install clean BATCH=YES
+cd /usr/ports/audio/hydrogen && make install clean OPTIONS="WITH_JACK" BATCH=YES
+cd /usr/ports/audio/yoshimi && make install clean BATCH=YES
+cd /usr/ports/audio/deadbeef && make install clean BATCH=YES
 
+## INSTALLS AUTOMOUNT
 echo ""
-
 echo "Enabling automount..."
-
 echo ""
-
 pkg install -y automount exfat-utils fusefs-exfat fusefs-ntfs fusefs-ext2 fusefs-hfsfuse fusefs-lkl fusefs-simple-mtpfs dsbmc
-
 echo ""
 
+## ENABLES BASIC SYSTEM SERVICES
 echo "Enabling basic services"
-
 sysrc moused_enable="YES"
 sysrc dbus_enable="YES"
 sysrc hald_enable="YES"
 sysrc slim_enable="YES"
 sysrc update_motd="NO"
 sysrc rc_startmsgs="NO"
-
 echo ""
 
+## CREATES .xinitrc SCRIPT FOR A REGULAR DESKTOP USER
 cd
-
 touch .xinitrc
-
 echo 'exec xfce4-session' >> .xinitrc
-
 echo ""
-
 echo ; read -p "Want to enable XFCE for a regular user? (yes/no): " X;
-
 echo ""
-
 if [ "$X" = "yes" ]
 then
     echo ; read -p "For what user? " user;
@@ -82,20 +77,22 @@ then
     echo "$user enabled"
 else fi
 
+## CONFIGURES AUTOMOUNT FOR THE REGULAR DESKTOP USER
 touch /usr/local/etc/automount.conf
 echo "USERUMOUNT=YES" >> /usr/local/etc/automount.conf
 echo "USER=$user" >> /usr/local/etc/automount.conf
 echo "FM='thunar'" >> /usr/local/etc/automount.conf
 echo "NICENAMES=YES" >> /usr/local/etc/automount.conf
 
+## SPECIAL PERMISSIONS FOR USB DRIVES AND WEBCAM
 echo "perm    /dev/da0        0666" >> /etc/devfs.conf
 echo "perm    /dev/da1        0666" >> /etc/devfs.conf
 echo "perm    /dev/da2        0666" >> /etc/devfs.conf
 echo "perm    /dev/da3        0666" >> /etc/devfs.conf
 echo "perm    /dev/video0     0666" >> /etc/devfs.conf
-
 echo ""
 
+## ADDS USER TO CORE GROUPS
 echo "Adding $user to video/realtime/wheel/operator groups"
 pw usermod $user -G video
 pw usermod $user -G realtime
@@ -103,27 +100,23 @@ pw usermod $user -G wheel
 pw usermod $user -G operator
 pw usermod $user -G network
 pw usermod $user -G webcamd
-
 echo ""
 
+## ADDS USER TO SUDOERS
 echo "Adding $user to sudo"
 echo "$user ALL=(ALL:ALL) ALL" >> /usr/local/etc/sudoers
-
 echo ""
 
+## ENABLES LINUX COMPAT LAYER
 echo "Enabling Linux compat layer..."
-
 echo ""
-
 kldload linux.ko
 sysrc linux_enable="YES"
-
 echo ""
 
+## COMPILE AND INSTALL LATEST NVIDIA DRIVERS
 echo "Compiling latest Nvidia drivers..."
-
 echo ""
-
 cd /usr/ports/x11/nvidia-driver
 make install clean BATCH=yes
 cd /usr/ports/x11/nvidia-settings
@@ -131,17 +124,13 @@ make install clean BATCH=yes
 cd /usr/ports/x11/nvidia-xconfig
 make install clean BATCH=yes
 nvidia-xconfig
-
 echo ""
-
 echo "Latest Nvidia drivers compiled"
-
 echo ""
 
+## FreeBSD SYSTEM TUNING FOR BEST DESKTOP EXPERIENCE
 echo "Optimizing system parameters and firewall..."
-
 echo ""
-
 mv /etc/sysctl.conf /etc/sysctl.conf.bk
 mv /boot/loader.conf /boot/loader.conf.bk
 mv /etc/login.conf /etc/login.conf.bk
@@ -155,10 +144,9 @@ touch /etc/pf.conf
 echo 'block in all' >> /etc/pf.conf
 echo 'pass out all keep state' >> /etc/pf.conf
 
+## CONFIGURES MORE CORE SYSTEM SERVICES
 echo "Enabling additional system services..."
-
 echo ""
-
 sysrc pf_enable="YES"
 sysrc pf_rules="/etc/pf.conf" 
 sysrc pf_flags=""
@@ -183,32 +171,26 @@ sysrc jackd_user="$user"
 sysrc jackd_rtprio="YES"
 sysrc jackd_args="-r -doss -r48000 -p512 -n1 -w16 \
             --capture /dev/dsp7 --playback /dev/dsp7"
-
 echo ""
 
+## UPDATES CPU MICROCODE
 echo "Updating CPU microcode..."
-
 echo ""
-
 pkg install -y devcpu-data
 sysrc microcode_update_enable="YES"
 service microcode_update start
-
 echo ""
-
 echo "Microcode updated"
-
 echo ""
 
+## CLEAN CACHES AND AUTOREMOVES UNNECESARY FILES
 echo "Cleaning system..."
-
 echo ""
-
 pkg clean -y
 pkg autoremove -y
-
 echo ""
 
+## DONE, PLEASE RESTART
 echo "Installation done"
 echo "Please restart your system."
 echo "BSD-XFCE by Wamphyre :)"
