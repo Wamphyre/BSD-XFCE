5251bfe090b8a14ac083c0a7d87a96712383c6e0
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2022-10-23 09:29:32 +0200

    Create creative_suite.sh

---
+++ creative_suite.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## Creative Suite for BSD-XFCE
+## Version 1.0
+
+test $? -eq 0 || exit 1 "NEED TO BE ROOT TO RUN THIS (YOU CAN USE SUDO TOO)"
+
+## UPDATE PACKAGES AND PORTS
+pkg update && portsnap fetch auto
+
+## COMPILE AND INSTALLS CREATIVE SUITE APPLICATIONS
+pkg install -y vlc gimp shotcut lsp-plugins-lv2 ardour6 qjackctl audacity guitarix-lv2 drumgizmo yoshimi hydrogen deadbeef
+cd /usr/ports/audio/lmms && make install clean OPTIONS="WITH_JACK" BATCH=YES
+echo ""
+
+echo "Creative Suite installed, let's make something good!"
