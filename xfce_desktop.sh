5fad66474ae50e293d324153728bb7f8df31f382
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2023-05-01 03:56:04 +0200

    Update xfce_desktop.sh

--- xfce_desktop.sh
+++ xfce_desktop.sh
@@ -36,7 +36,7 @@ echo "Installing XFCE..."
 echo ""
 pkg install -y nano sudo bash wget htop xorg slim xfce xfce4-pulseaudio-plugin thunar-archive-plugin xarchiver unzip 
 pkg install -y gnome-keyring xfce4-screenshooter-plugin shotwell atril-lite gnome-font-viewer mixer mixertui vlc qjackctl  
-pkg install -y baobab networkmgr v4l-utils v4l_compat webcamd pwcview sctd brut firefox deadbeef
+pkg install -y baobab networkmgr v4l-utils v4l_compat webcamd pwcview sctd brut firefox deadbeef fuse
 
 ## INSTALLS AUTOMOUNT AND FILESYSTEM SUPPORT
 echo ""
