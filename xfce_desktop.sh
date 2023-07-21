a87632edbc032d3c11e797325f0955de5b8f21e3
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2023-07-21 12:08:51 +0200

    Update xfce_desktop.sh

--- xfce_desktop.sh
+++ xfce_desktop.sh
@@ -99,22 +99,6 @@ kldload linux.ko
 sysrc linux_enable="YES"
 echo ""
 
-## COMPILE AND INSTALL LATEST NVIDIA DRIVERS FOR MODERN CARDS (GTX 1*** / RTX Series)
-## Please, change /usr/ports/x11/nvidia-driver to /usr/ports/x11/nvidia-driver-470 or nvidia-driver-390 if you use GeForce GT or Titan series card.
-## If you're using a legacy Nvidia GeForce card please use nvidia-driver-304 instead
-echo "Compiling Nvidia drivers..."
-echo ""
-cd /usr/ports/x11/nvidia-driver
-make install clean BATCH=yes
-cd /usr/ports/x11/nvidia-settings
-make install clean BATCH=yes
-cd /usr/ports/x11/nvidia-xconfig
-make install clean BATCH=yes
-nvidia-xconfig
-echo ""
-echo "Nvidia drivers compiled"
-echo ""
-
 ## FreeBSD SYSTEM TUNING FOR BEST DESKTOP EXPERIENCE
 echo "Optimizing system parameters and firewall..."
 echo ""
