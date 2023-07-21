eb6096aab391d0e37f03bb6de0b42b088a3d12c8
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2023-07-21 19:30:33 +0200

    Update intel_gpu_install.sh

--- intel_gpu_install.sh
+++ intel_gpu_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-## If you notice high CPU usage, or excessive tearing with HD video: multimedia/libva-intel-driver may help – installation should be in addition to drm-kmod, mesa-libs and mesa-dri.
-
 pkg install -y drm-kmod libva-intel-driver
 sysrc -f /etc/rc.conf kld_list+=i915kms
 
